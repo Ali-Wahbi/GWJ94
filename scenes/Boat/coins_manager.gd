a4501de8f85578f3205d6a330bea786d2813efec
Ali-Wahbi <aliwahbi436@gmail.com>
2026-06-15 15:34:14 +0300

    temp

--- scenes/Boat/coins_manager.gd
+++ scenes/Boat/coins_manager.gd
@@ -1,19 +1,21 @@
 extends Node2D
 
-var currentCoins: int = 100
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		collectCoin(10)
+	
+	if Input.is_action_just_pressed("ui_accept") and Input.is_action_just_pressed("Right"):
+		collectCoin(10)
+	
+
 func collectCoin(value: int) -> void:
-	currentCoins += value
-
-func consumeCoin(value: int) -> void:
-	currentCoins -= value
-
-func getCurrentCoins() -> int:
-	return currentCoins
+	# UiData.CollectCoins(value)
+	pass
 
 func updateUI() -> void:
 	pass # Replace with function body.
